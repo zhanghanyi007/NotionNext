35231a5d501abbb000c8ffc37883553939ef853f
zhanghanyi007 <139729343+zhanghanyi007@users.noreply.github.com>
2024-08-19 13:25:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,13 +20,13 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '读万卷书,行万里路,要阅人无数',
+    '读万卷书，行万里路，要阅人无数',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '筱戶', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '市场的摆渡人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://fishonly.eu.org', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
